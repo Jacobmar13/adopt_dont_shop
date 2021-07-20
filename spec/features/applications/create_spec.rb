d247329540b714cfa49dd6f28dba9677d5ae1c3d
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-19 18:32:04 -0600

    Add tests for confirming form fields

---
+++ spec/features/applications/create_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'application creation' do
+  describe 'start an application' do
+    it 'renders the new form' do
+      visit '/applications/new'
+
+      expect(page).to have_content('New Application')
+      expect(find('form')).to have_content('Name')
+      expect(find('form')).to have_content('Street address')
+      expect(find('form')).to have_content('City')
+      expect(find('form')).to have_content('State')
+      expect(find('form')).to have_content('Zipcode')
+      expect(find('form')).to have_content('Your reason for adopting')
+    end
+  end
+end
