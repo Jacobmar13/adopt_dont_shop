b26eb9d66e4f9b5117732d4d3a5ab6af12ae075e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-19 19:06:52 -0600

    Add tests to confirm form can be filled

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe 'application creation' do
       expect(find('form')).to have_content('Your reason for adopting')
     end
   end
+
+  describe 'the application create' do
+    context 'given valid data' do
+      it 'creates the application and redirects to the application show page' do
+        visit '/applications/new'
+
+        fill_in 'Name', with: 'Jacob'
+        fill_in 'Street address', with: '1234 Sunshine Blvd'
+        fill_in 'City', with: 'Denver'
+        fill_in 'State', with: 'CO'
+        fill_in 'Zipcode', with: '12345'
+        fill_in 'Your reason for adopting', with: 'I really like dogs'
+        click_button 'Create Application'
+
+        expect(page).to have_content('Jacob')
+        expect(page).to have_content('1234 Sunshine Blvd')
+        expect(page).to have_content('Denver')
+        expect(page).to have_content('CO')
+        expect(page).to have_content('12345')
+        expect(page).to have_content('I really like dogs')
+        expect(page).to have_content('In Progress')
+      end
+    end
+  end
 end
