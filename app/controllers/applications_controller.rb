9614c47514fae66e73699e63429b935b81757b13
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-19 19:07:18 -0600

    Add create action to make new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,5 +4,25 @@ class ApplicationsController < ApplicationController
   end
 
   def new
+    @application = Application.new
+  end
+
+  def create
+    application = Application.create!(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.require(:application).permit(
+      :name,
+      :street_address,
+      :city,
+      :state,
+      :zip_code,
+      :applicant_story,
+      :application_status
+    )
   end
 end
