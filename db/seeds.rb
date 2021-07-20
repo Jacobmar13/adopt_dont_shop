5e7c91f90944f41c60c1a8f64a8eb17d7f833f4f
Jacob Martinez <jacobmarcareer@gmail.com>
2021-07-20 16:44:35 -0600

    Add seeds to use with DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.destroy_all
+Shelter.destroy_all
+
+shelter = Shelter.create!(
+  name: 'Mystery Building',
+  city: 'Irvine CA',
+  foster_program: false,
+  rank: 9
+)
+
+Pet.create(
+  name: 'Scooby',
+  age: 2,
+  breed: 'Great Dane',
+  adoptable: true,
+  shelter_id: shelter.id
+)
+Pet.create(
+  name: 'Bacardi',
+  age: 6,
+  breed: 'Daschund',
+  adoptable: true,
+  shelter_id: shelter.id
+)
+Pet.create(
+  name: 'Yaeger',
+  age: 1,
+  breed: 'Swedish Valhund',
+  adoptable: true,
+  shelter_id: shelter.id
+)
